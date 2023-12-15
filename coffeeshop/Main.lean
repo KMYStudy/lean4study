c6d3c2308f36ca25f6e578b88f629dfde92f6e86
kremilly <theemilysilva@outlook.com>
2023-12-14 23:44:06 -0300

    feat: lists, instance and classes

--- coffeeshop/Main.lean
+++ coffeeshop/Main.lean
@@ -57,5 +57,53 @@ def meal : Product :=
 def donut : Product :=
   Product.donut myFavoriteDonut
 
-def main : IO Unit :=
-  IO.println "Hello world"
+inductive Lista
+  | nil
+  | cons (head: Nat) (tail: Lista)
+
+def ListaSize : Lista -> Nat
+  | Lista.nil => 0
+  | Lista.cons _ tail => 1 + ListaSize tail
+
+def bigList : Lista := Lista.cons 1 (Lista.cons 2 (Lista.cons 3 Lista.nil))
+
+def emptyList : Lista := Lista.nil
+
+def threeElementsList : Lista := Lista.cons 1 (Lista.cons 2 (Lista.cons 3 Lista.nil))
+
+def listLength : Lista -> Nat
+  | Lista.nil => 0
+  | Lista.cons _ tail => 1 + listLength tail
+
+#eval listLength Lista.nil
+#eval listLength threeElementsList
+
+def printList : Lista -> String
+  | Lista.nil => ""
+  | Lista.cons head tail => toString head ++ " " ++ printList tail
+
+#eval printList threeElementsList
+
+def concatList : Lista -> Lista -> Lista
+  | Lista.nil, y => y
+  | Lista.cons x xs, y => Lista.cons x (concatList xs y)
+
+class Priceable (α: Type) where
+  price : α -> Nat
+
+instance : Priceable Coffee where
+  price
+    | Coffee.espresso => 2
+    | Coffee.cappuccino => 3
+    | Coffee.latte => 3
+    | Coffee.mocha => 4
+
+instance : Priceable Donut where
+  price donut :=
+    let flavorPrice :=
+      match donut.flavor with
+        | Flavor.vanilla => 1
+        | Flavor.chocolate => 2
+        | Flavor.strawberry => 2
+    let sprinklePrice := if donut.sprinkles then 1 else 0
+    flavorPrice + sprinklePrice
